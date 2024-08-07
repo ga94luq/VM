52c95d0f1e96fda9ce7c7c7bc4776262617d5e2c
ga94luq <161119215+ga94luq@users.noreply.github.com>
2024-08-07 14:42:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,21 +7,31 @@ import dash_bootstrap_components as dbc
 # Erstellen Sie die Dash-App und fügen Sie ein Bootstrap-Theme hinzu
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
+
 # Layout der Dash-App
 app.layout = html.Div([
     html.H1("Timer für Namen"),
     dcc.RadioItems(
         id='name-radioitems',
         options=[
-            {'label': 'Name 1', 'value': 'Name1'},
-            {'label': 'Name 2', 'value': 'Name2'},
-            {'label': 'Name 3', 'value': 'Name3'},
-            {'label': 'Name 4', 'value': 'Name4'},
-            {'label': 'Name 5', 'value': 'Name5'}
+            {'label': 'Andreas Noel', 'value': 'Name1'},
+            {'label': 'Simon Mailhammer', 'value': 'Name2'},
+            {'label': 'Maximilian Müller', 'value': 'Name3'},
+            {'label': 'Simon Hofmann', 'value': 'Name4'},
+            {'label': 'Anderer Benutzer', 'value': 'Name5'}
         ],
         value='Name1'  # Standardwert
     ),
-    html.H3("Verstrichene Zeit seit Auswahl (Ablauf von 2h):"),
+    html.H3("Wählen Sie die Zeitspanne (.5 bis 6 Stunden):"),
+    dcc.Slider(
+        id='time-slider',
+        min=.5,
+        max=6,
+        step=0.1,
+        value=2,  # Startwert in Stunden
+        marks={i: f'{i}h' for i in range(1, 7)}
+    ),
+    html.H3("Verstrichene Zeit seit Auswahl:"),
     html.Div(id='timer-display'),
     dbc.Progress(id='progress-bar', value=0, max=100, striped=True, animated=True, style={"height": "30px"}),
     dcc.Interval(
@@ -33,17 +43,22 @@ app.layout = html.Div([
 
 # Globale Variable zum Speichern der Startzeit
 start_time = None
-two_hours = timedelta(hours=2)
+time_limit = timedelta(hours=2)
 
 # Callback-Funktion zum Starten des Timers bei Änderung des Namens
 @app.callback(
     [Output('timer-display', 'children'),
      Output('progress-bar', 'value')],
     [Input('interval-component', 'n_intervals')],
-    [State('name-radioitems', 'value')]
+    [State('name-radioitems', 'value'),
+     State('time-slider', 'value')]
 )
-def update_timer(n_intervals, selected_name):
+def update_timer(n_intervals, selected_name, slider_value):
     global start_time
+
+    # Berechnen der Zeitspanne basierend auf dem Slider-Wert
+    global time_limit
+    time_limit = timedelta(hours=slider_value)
 
     if n_intervals == 0 or start_time is None:
         return "00:00:00", 0
@@ -53,7 +68,7 @@ def update_timer(n_intervals, selected_name):
     elapsed_str = str(elapsed_time).split('.')[0]  # Formatieren der Zeit
 
     # Berechnung des Fortschritts
-    progress_percentage = min((elapsed_time / two_hours) * 100, 100)
+    progress_percentage = min((elapsed_time / time_limit) * 100, 100)
 
     return elapsed_str, progress_percentage
 
