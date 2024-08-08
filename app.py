fe5407ad2c9be66af8f22556cca225436f926d19
ga94luq <161119215+ga94luq@users.noreply.github.com>
2024-08-08 08:35:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
 from datetime import datetime, timedelta
+import dash_bootstrap_components as dbc
 
-# Create the Dash app
 app = dash.Dash(__name__)
 server = app.server
 
-# Layout of the Dash app
 app.layout = html.Div([
     html.H1("Timer für Namen"),
     dcc.RadioItems(
@@ -19,7 +18,7 @@ app.layout = html.Div([
             {'label': 'Simon Hofmann', 'value': 'Name4'},
             {'label': 'Anderer Benutzer', 'value': 'Name5'}
         ],
-        value='Name1'  # Default value
+        value='Name1'
     ),
     html.H3("Wählen Sie die Zeitspanne (.5 bis 6 Stunden):"),
     dcc.Slider(
@@ -27,44 +26,42 @@ app.layout = html.Div([
         min=.5,
         max=6,
         step=0.1,
-        value=2,  # Start value in hours
+        value=2,
         marks={i: f'{i}h' for i in range(1, 7)}
     ),
     html.H3("Verstrichene Zeit seit Auswahl:"),
     html.Div(id='timer-display'),
+    dbc.Progress(id='progress-bar', value=0, max=100, striped=True, animated=True, style={"height": "30px", 'width':'1000px'}),
     dcc.Interval(
         id='interval-component',
-        interval=1000,  # 1 second in milliseconds
+        interval=1000,
         n_intervals=0
     )
 ])
 
-# Global variable to store the start time
 start_time = None
 time_limit = timedelta(hours=2)
 
-# Callback function to start the timer when the name is changed
 @app.callback(
-    Output('timer-display', 'children'),
+    [Output('timer-display', 'children'),
+     Output('progress-bar', 'value')],
     [Input('interval-component', 'n_intervals')],
     [State('name-radioitems', 'value'),
      State('time-slider', 'value')]
 )
 def update_timer(n_intervals, selected_name, slider_value):
     global start_time
-
-    # Calculate the time span based on the slider value
     global time_limit
     time_limit = timedelta(hours=slider_value)
 
     if n_intervals == 0 or start_time is None:
-        return "00:00:00"
+        return "00:00:00", 0
 
-    # Calculate the elapsed time
     elapsed_time = datetime.now() - start_time
-    elapsed_str = str(elapsed_time).split('.')[0]  # Format the time
+    elapsed_str = str(elapsed_time).split('.')[0]
+    progress_percentage = min((elapsed_time / time_limit) * 100, 100)
 
-    return elapsed_str
+    return elapsed_str, progress_percentage
 
 @app.callback(
     Output('interval-component', 'n_intervals'),
@@ -72,9 +69,8 @@ def update_timer(n_intervals, selected_name, slider_value):
 )
 def start_timer(selected_name):
     global start_time
-    start_time = datetime.now()  # Set the start time
-    return 0  # Reset the interval component
+    start_time = datetime.now()
+    return 0
 
-# Start the Dash app
 if __name__ == '__main__':
     app.run_server(debug=True)
